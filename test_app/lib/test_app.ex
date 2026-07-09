eed012105e204b0987c031beaf395a33fa7e76a7
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-07-08 21:37:18 -0300

    Updated and improved core app

--- test_app/lib/test_app.ex
+++ test_app/lib/test_app.ex
@@ -1,5 +1,18 @@
 defmodule TestApp do
+  defp backend, do: Application.fetch_env!(:test_app, :backend)
+
   def main() do
     IO.puts("Hello from TestApp!")
+
+    IO.puts("Checking which backend is being used...")
+
+    case backend() do
+      nil ->
+        IO.puts("No backend configured.")
+
+      backend ->
+        IO.puts("Using backend: #{backend}")
+        backend.hello()
+    end
   end
 end
