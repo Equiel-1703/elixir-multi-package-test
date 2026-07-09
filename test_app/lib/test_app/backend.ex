9ced91d9973e36bccbaef0f077da9d0b6894a260
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-07-08 23:26:04 -0300

    Updated backend behaviour

--- test_app/lib/test_app/backend.ex
+++ test_app/lib/test_app/backend.ex
@@ -1,4 +1,6 @@
 # This is a behaviour that all backends must implement.
 defmodule TestApp.Backend do
   @callback hello() :: atom()
+
+  @callback sum(a :: integer(), b :: integer()) :: integer()
 end
