9d681c1701e418dca444dfa1edce451bfe7f4fe7
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-07-08 23:23:56 -0300

    Since the main app is not supposed to run by itself, I deleted the test folder, config and dummy backend

--- test_app/config/config.exs
+++
@@ -1,6 +0,0 @@
-import Config
-
-# This config is only to keep ElixirLS happy. The end user should
-# provide it's own config file. The DummyBackend is just a placeholder
-# to avoid compilation errors on development.
-config :test_app, backend: TestApp.DummyBackend
