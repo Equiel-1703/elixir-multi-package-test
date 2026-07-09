283bdb4838eb973ce4f21a0961cb4a17e3c06ca0
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-07-08 23:38:25 -0300

    Refactor output formatting in TestApp and 'do_math' call

--- test_app/lib/test_app.ex
+++ test_app/lib/test_app.ex
@@ -2,16 +2,16 @@ defmodule TestApp do
   defp backend, do: Application.fetch_env!(:test_app, :backend)
 
   def main() do
-    IO.puts("Hello from TestApp!")
+    IO.puts("-- Hello from TestApp! --")
     IO.puts("Checking which backend is being used...")
 
-    IO.puts("Using backend: #{backend()}")
+    IO.puts("\n-> Using backend: #{backend()}\n")
     backend().hello()
 
-    IO.puts("Adding two numbers using the backend's sum function...")
-    _result = backend().sum(3, 5)
+    IO.puts("\nDoing some math using the backend...")
+    _result = backend().do_math(5)
 
-    IO.puts("End of TestApp execution.")
+    IO.puts("\n-- End of TestApp execution --")
 
     :ok
   end
